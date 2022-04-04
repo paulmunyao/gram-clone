f899b9818f7ae10b17d0b52333870f6500dba93b
Paul Munyao <paulmunyao094@gmail.com>
2022-04-05 02:44:37 +0300

    Configuring url paths to our project for authentification

--- metagram/forms.py
+++ metagram/forms.py
@@ -4,9 +4,7 @@ class Signupform(forms.Form):
     email = forms.EmailField(label='email', max_length=100)
     full_name = forms.CharField(label='full_name', max_length=100)
     user_name = forms.CharField(label='username', max_length=100)
-    password = forms.CharField(label='password', max_length=100)
-    
-    
+    password = forms.CharField(label='password', max_length=100) 
 class Loginform(forms.Form):
     user_name = forms.CharField(label='username', max_length=100)
     password = forms.CharField(label='password', max_length=100)    
