a56fabce77ff439c6644dae2826f688f66ae2f64
Paul Munyao <paulmunyao094@gmail.com>
2022-04-05 02:33:05 +0300

    Rename signup forms

--- metagram/forms.py
+++ metagram/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class Inputform(forms.Form):
+class Signupform(forms.Form):
     user_name = forms.CharField(label='username', max_length=100)
     password = forms.CharField(label='password', max_length=100)
     email = forms.EmailField(label='email', max_length=100)
