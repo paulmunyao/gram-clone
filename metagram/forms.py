b606eb4520e2900c9ea8bcfb669320727edf2d1b
Paul Munyao <paulmunyao094@gmail.com>
2022-04-05 02:33:57 +0300

    Rearrange the position of the forms

--- metagram/forms.py
+++ metagram/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 
 class Signupform(forms.Form):
-    user_name = forms.CharField(label='username', max_length=100)
-    password = forms.CharField(label='password', max_length=100)
     email = forms.EmailField(label='email', max_length=100)
     full_name = forms.CharField(label='full_name', max_length=100)
+    user_name = forms.CharField(label='username', max_length=100)
+    password = forms.CharField(label='password', max_length=100)
+    
     
 class Loginform(forms.Form):
     user_name = forms.CharField(label='username', max_length=100)
