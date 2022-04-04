1ea85a8e53e90f9f806480fcc7e2c736ec1d1fd0
Paul Munyao <paulmunyao094@gmail.com>
2022-04-05 01:21:43 +0300

    Create models for user

--- metagram/models.py
+++ metagram/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=50)
+    full_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    
+    
