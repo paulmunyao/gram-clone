d19f9d5921ac73d048fc1f7e49f19e6b573de9ab
Paul Munyao <paulmunyao094@gmail.com>
2022-04-05 02:04:11 +0300

    Using login for authentification

--- metagram/views.py
+++ metagram/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import Inputform, authenticate
+from .forms import User, authenticate,login
 
 # Create your views here.
 
@@ -11,13 +11,14 @@ def home(request):
 def login(request):
     username = request.POST['username']
     password = request.POST['password']
-    User = authenticate(username=username, password=password)
+    username = authenticate(username=username, password=password)
     if User is not None:
+        login(request, User)
         # redirect to home page when user is logged in
         return render(request, "home.html")
-        else:
-            # redirect to login page when user is not logged in
-            return render(request, "login.html", {'form': Inputform()})
+    else:
+    # redirect to login page when user is not logged in
+        return render(request, "login.html", {'form': User()})
 
 
 def signup(request):
